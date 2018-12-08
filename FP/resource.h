3b8007368c639a1f8aa87f22054fe9e50719f471
Bonnis <rafti.f@gmail.com>
2018-12-08 14:46:32 +0700

    Nambah Asset untuk setiap tank -KV2 -Pershing -Sherman -Panther -T34

--- FP/resource.h
+++ FP/resource.h
@@ -9,12 +9,20 @@
 #define PRSHG_GUN						105
 #define TIGER_BODY						106
 #define TIGER_GUN						107
+#define KV2_BODY						108
+#define KV2_GUN							109
+#define SHRMN_BODY						110
+#define SHRMN_GUN						111
+#define T34_BODY						112
+#define T34_GUN							113
+#define PNTHR_BODY						114
+#define PNTHR_GUN						115
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        116
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           103
